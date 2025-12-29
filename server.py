881a649f54a826cc3e2a96c07367ccc3c7030612
Stein Petter Tokvam <stto1210@gmail.com>
2025-12-29 21:14:46 +0100

    process json for weather.json endpoint

--- server.py
+++ server.py
@@ -6,15 +6,57 @@ import netatmo
 import weather
 import logging
 import os 
+import utils
 
 logging.basicConfig()
 logging.root.setLevel(logging.INFO)
 
 serverLogger = logging.getLogger("server")
 
+
 class WeatherHandler(http.server.SimpleHTTPRequestHandler):
+    def read_and_process_files(self):
+        global weather_data
+        weather_data = {}
+        # Read weather data
+        if os.path.isfile(weather.weather_data_filename):
+            yr_data = {}
+            yr_data = utils.read_json(weather.weather_data_filename)["properties"]["timeseries"]
+            filtered_weather_data = []
+            index = 0
+            for timeseries in yr_data:
+                if len(yr_data) >= 18 and (index == 0 or index == 6 or index == 12 or index == 18):
+                    curr_timeseries = {}
+                    curr_timeseries["time"] = timeseries["time"]
+                    if "next_6_hours" in timeseries["data"]:
+                        curr_timeseries["summary"] = timeseries["data"]["next_6_hours"]["summary"]["symbol_code"]
+                        curr_timeseries["min_temp"] = timeseries["data"]["next_6_hours"]["details"]["air_temperature_min"]
+                        curr_timeseries["max_temp"] = timeseries["data"]["next_6_hours"]["details"]["air_temperature_max"]
+                        curr_timeseries["precipitation_amount"] = timeseries["data"]["next_6_hours"]["details"]["precipitation_amount"]
+                        curr_timeseries["precipitation_amount_max"] = timeseries["data"]["next_6_hours"]["details"]["precipitation_amount_max"]
+                        curr_timeseries["precipitation_amount_min"] = timeseries["data"]["next_6_hours"]["details"]["precipitation_amount_min"]
+
+                    filtered_weather_data.append(curr_timeseries)
+                index += 1
+            weather_data["yr"] = filtered_weather_data
+        else:
+            serverLogger.error("No weather data file")
+            weather_data = {}
+        if os.path.isfile(netatmo.data_filename):
+            netatmo_data = utils.read_json(netatmo.data_filename)
+            filtered_netatmo_data = netatmo_data["body"]["devices"]
+            dashboard_data = [filtered_netatmo_data[0]["dashboard_data"]]
+            dashboard_data[0]["module_type"] = "NAMain"
+
+            for device in filtered_netatmo_data[0]["modules"]:
+                if "dashboard_data" in device:
+                    dashboard_data.append(device["dashboard_data"])
+                    dashboard_data[-1]["module_type"] = device["type"]
+            weather_data["netatmo"] = dashboard_data
+
     def do_GET(self):
         if self.path == "/weather.json":
+            self.read_and_process_files()
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
